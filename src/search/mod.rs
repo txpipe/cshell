61d3fa2183502eb54ceffe08140a03909bb954c2
paulobressan <paulo.bressan@outlook.com>
2025-05-16 10:40:07 -0300

    feat: implemented fetch transaction

--- src/search/mod.rs
+++ src/search/mod.rs
@@ -1,11 +1,12 @@
 use clap::{command, Parser, Subcommand};
 use comfy_table::Table;
 use tracing::instrument;
-use utxorpc::spec::sync::{any_chain_block, AnyChainBlock};
+use utxorpc::spec::{cardano::Tx, query, sync};
 
 use crate::output::OutputFormatter;
 
 mod block;
+mod transaction;
 
 #[derive(Parser)]
 pub struct Args {
@@ -17,76 +18,118 @@ pub struct Args {
 enum Commands {
     /// fetch block
     Block(block::Args),
+
+    /// fetch transaction
+    Transaction(transaction::Args),
 }
 
 #[instrument("search", skip_all)]
 pub async fn run(args: Args, ctx: &mut crate::Context) -> miette::Result<()> {
     match args.command {
         Commands::Block(args) => block::run(args, ctx).await,
+        Commands::Transaction(args) => transaction::run(args, ctx).await,
     }
 }
 
-impl OutputFormatter for Vec<AnyChainBlock> {
+fn cardano_tx_table(block_hash: Vec<u8>, tx: &[Tx]) -> Table {
+    let mut table = Table::new();
+    table.set_header(vec![
+        "Block",
+        "",
+        "Hash",
+        "Inputs",
+        "Outputs",
+        "Certificates",
+        "Ref Inputs",
+        "Datum",
+    ]);
+
+    let block_hash = hex::encode(block_hash);
+    let block_hash_trucated = format!(
+        "{}...{}",
+        &block_hash[..4],
+        &block_hash[block_hash.len() - 4..]
+    );
+
+    for (i, tx) in tx.iter().enumerate() {
+        let hash = hex::encode(&tx.hash);
+        let inputs = tx.inputs.len();
+        let outputs = tx.outputs.len();
+        let certificates = tx.certificates.len();
+        let reference_inputs = tx.reference_inputs.len();
+
+        let contains_datum = if tx.outputs.iter().any(|o| {
+            o.datum
+                .as_ref()
+                .map(|d| !d.hash.is_empty())
+                .unwrap_or_default()
+        }) {
+            "contain"
+        } else {
+            "empty"
+        };
+
+        table.add_row(vec![
+            &block_hash_trucated,
+            &i.to_string(),
+            &hash,
+            &inputs.to_string(),
+            &outputs.to_string(),
+            &certificates.to_string(),
+            &reference_inputs.to_string(),
+            contains_datum,
+        ]);
+    }
+
+    table
+}
+
+impl OutputFormatter for Vec<sync::AnyChainBlock> {
     fn to_table(&self) {
         for block in self {
             if let Some(chain) = &block.chain {
                 match chain {
-                    any_chain_block::Chain::Cardano(block) => {
-                        let mut table = Table::new();
-                        table.set_header(vec![
-                            "Block",
-                            "",
-                            "Hash",
-                            "Inputs",
-                            "Outputs",
-                            "Certificates",
-                            "Ref Inputs",
-                            "Datum",
-                        ]);
-
+                    sync::any_chain_block::Chain::Cardano(block) => {
                         if block.header.is_none() {
                             return;
                         }
-
                         let header = block.header.as_ref().unwrap();
-                        let block_hash = hex::encode(&header.hash);
-                        let block_hash_trucated = format!(
-                            "{}...{}",
-                            &block_hash[..4],
-                            &block_hash[block_hash.len() - 4..]
-                        );
-
                         if let Some(body) = &block.body {
-                            for (i, tx) in body.tx.iter().enumerate() {
-                                let hash = hex::encode(&tx.hash);
-                                let inputs = tx.inputs.len();
-                                let outputs = tx.outputs.len();
-                                let certificates = tx.certificates.len();
-                                let reference_inputs = tx.reference_inputs.len();
+                            let table = cardano_tx_table(header.hash.clone().into(), &body.tx);
+                            println!("{table}");
+                        }
+                    }
+                }
+            }
+        }
+    }
 
-                                let contains_datum = if tx.outputs.iter().any(|o| {
-                                    o.datum
-                                        .as_ref()
-                                        .map(|d| !d.hash.is_empty())
-                                        .unwrap_or_default()
-                                }) {
-                                    "contain"
-                                } else {
-                                    "empty"
-                                };
+    fn to_json(&self) {
+        let result = serde_json::to_value(self);
+        if let Err(err) = result {
+            eprintln!("{err}");
+            return;
+        }
 
-                                table.add_row(vec![
-                                    &block_hash_trucated,
-                                    &i.to_string(),
-                                    &hash,
-                                    &inputs.to_string(),
-                                    &outputs.to_string(),
-                                    &certificates.to_string(),
-                                    &reference_inputs.to_string(),
-                                    contains_datum,
-                                ]);
-                            }
+        println!(
+            "{}",
+            serde_json::to_string_pretty(&result.unwrap()).unwrap()
+        );
+    }
+}
 
+impl OutputFormatter for Vec<query::AnyChainBlock> {
+    fn to_table(&self) {
+        for block in self {
+            if let Some(chain) = &block.chain {
+                match chain {
+                    query::any_chain_block::Chain::Cardano(block) => {
+                        if block.header.is_none() {
+                            return;
+                        }
+                        let header = block.header.as_ref().unwrap();
+                        if let Some(body) = &block.body {
+                            let table = cardano_tx_table(header.hash.clone().into(), &body.tx);
                             println!("{table}");
                         }
                     }
---
+++ src/search/transaction.rs
@@ -0,0 +1,43 @@
+use clap::Parser;
+use miette::{bail, Context, IntoDiagnostic, Result};
+use tracing::instrument;
+
+use crate::output::OutputFormatter;
+
+#[derive(Parser)]
+pub struct Args {
+    /// Transaction hash
+    #[arg(required = true, help = "Transaction hash")]
+    hash: String,
+
+    /// Name of the provider to use. If undefined, will use default
+    #[arg(long, help = "Name of the provider to use")]
+    provider: Option<String>,
+}
+
+#[instrument(skip_all, name = "block")]
+pub async fn run(args: Args, ctx: &mut crate::Context) -> Result<()> {
+    let provider = match args.provider {
+        Some(name) => ctx.store.find_provider(&name),
+        None => ctx.store.default_provider(),
+    };
+
+    let Some(provider) = provider else {
+        bail!("Provider not found")
+    };
+
+    let hash = hex::decode(args.hash)
+        .into_diagnostic()
+        .context("invalid transaction hash")?;
+
+    match provider.fetch_tx(hash).await? {
+        Some(v) => {
+            if let Some(block) = v.block {
+                vec![block].output(&ctx.output_format)
+            }
+        }
+        None => bail!("transaction hash not found"),
+    }
+
+    Ok(())
+}
