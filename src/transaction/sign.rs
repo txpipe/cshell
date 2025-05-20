7c0de4554d41f79577a6752516adffa2ee1dc2df
paulobressan <paulo.bressan@outlook.com>
2025-05-20 18:29:32 -0300

    feat: implemented param to allow unsafe signatures

--- src/transaction/sign.rs
+++ src/transaction/sign.rs
@@ -11,6 +11,10 @@ pub struct Args {
     /// Transaction cbor
     cbor: String,
 
+    /// Allow sign using unsafe wallets
+    #[arg(long, help = "Allow unsafe wallet signatures")]
+    r#unsafe: bool,
+
     #[arg(long, help = "Wallets that will sign the transaction")]
     signer: Vec<String>,
 }
@@ -39,22 +43,38 @@ pub async fn run(args: Args, ctx: &crate::Context) -> miette::Result<()> {
         args.signer.clone()
     };
 
-    for signer in signers {
-        let wallet = ctx
-            .store
-            .wallets()
-            .iter()
-            .find(|wallet| wallet.name.to_string() == signer);
+    let wallets = signers
+        .iter()
+        .map(|signer| {
+            let wallet = ctx
+                .store
+                .wallets()
+                .iter()
+                .find(|wallet| wallet.name.to_string().eq(signer));
 
-        let Some(wallet) = wallet else {
-            bail!("invalid signer wallet {signer}")
-        };
+            let Some(wallet) = wallet else {
+                bail!("invalid signer wallet '{signer}'")
+            };
 
+            if wallet.is_unsafe && !args.r#unsafe {
+                bail!(
+                    "wallet '{signer}' is unsafe, use the param --unsafe to allow unsafe signatures"
+                )
+            }
+
+            Ok(wallet)
+        })
+        .collect::<Result<Vec<_>, _>>()?;
+
+    for wallet in wallets {
         let password = match wallet.is_unsafe {
             true => None,
             false => Some(
                 inquire::Password::new("Password:")
-                    .with_help_message(&format!("The spending password of your wallet {signer}:"))
+                    .with_help_message(&format!(
+                        "The spending password for '{}' wallet:",
+                        wallet.name
+                    ))
                     .with_display_mode(inquire::PasswordDisplayMode::Masked)
                     .prompt()
                     .into_diagnostic()?,
