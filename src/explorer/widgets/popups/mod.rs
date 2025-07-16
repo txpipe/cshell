68be5b30f690ac0d3ea529cd740aa1893ea99cd0
Paulo Bressan <paulo.bressan@outlook.com>
2025-07-16 13:20:52 -0300

    chore: set fixed popup window length (#33)

--- src/explorer/widgets/popups/mod.rs
+++ src/explorer/widgets/popups/mod.rs
@@ -1,24 +1,14 @@
-use ratatui::layout::{Constraint, Direction, Layout, Rect};
+use ratatui::layout::{Constraint, Flex, Layout, Rect};
 
 pub mod help;
 pub mod new_address;
 
-fn centered_rect(percent_x: u16, percent_y: u16, r: Rect) -> Rect {
-    let popup_layout = Layout::default()
-        .direction(Direction::Vertical)
-        .constraints([
-            Constraint::Percentage((100 - percent_y) / 2),
-            Constraint::Percentage(percent_y),
-            Constraint::Percentage((100 - percent_y) / 2),
-        ])
-        .split(r);
+fn centered_rect(length_x: u16, length_y: u16, area: Rect) -> Rect {
+    let horizontal = Layout::horizontal([Constraint::Length(length_x)]).flex(Flex::Center);
+    let vertical = Layout::vertical([Constraint::Length(length_y)]).flex(Flex::Center);
 
-    Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Percentage((100 - percent_x) / 2),
-            Constraint::Percentage(percent_x),
-            Constraint::Percentage((100 - percent_x) / 2),
-        ])
-        .split(popup_layout[1])[1]
+    let [area] = vertical.areas(area);
+    let [area] = horizontal.areas(area);
+
+    area
 }
